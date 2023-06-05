73f31a811739ad28bc443ac73b9cdd7993ae4e9a
Emre Turan <itsemreturan@gmail.com>
2023-06-06 00:46:18 +0300

    UI Improvements - Added animations

--- components/About.jsx
+++ components/About.jsx
@@ -48,11 +48,10 @@ const About = () => {
             </Link>
           </Fade>
         </div>
-        <Fade direction="right" triggerOnce="true">
-          <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-2 hover:scale-105 ease-in duration-300">
-            <Image src={AboutImg} className="rounded-xl" alt="/" />
-          </div>
-        </Fade>
+
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-2 hover:scale-105 ease-in duration-300">
+          <Image src={AboutImg} className="rounded-xl" alt="/" />
+        </div>
       </div>
     </div>
   );
