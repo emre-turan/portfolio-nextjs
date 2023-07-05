d95196d80642821accd66b48ceb65cc458fb897b
Emre Turan <itsemreturan@gmail.com>
2023-07-06 00:41:21 +0300

    organized assets

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import AboutImg from "../public/assets/about-3.png";
+import AboutImg from "../public/assets/about.png";
 import { Fade } from "react-awesome-reveal";
 
 const About = () => {
