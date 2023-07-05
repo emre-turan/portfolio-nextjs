aac975431941d3cb8dc8219d1850d86c63385e93
Emre Turan <itsemreturan@gmail.com>
2023-07-05 15:08:05 +0300

    font optimization

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,18 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
+import { Urbanist } from "next/font/google";
 
+const font = Urbanist({ subsets: ["latin"] });
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Navbar />
-      <Component {...pageProps} />
-      <Analytics />
-    </ThemeProvider>
+    <main className={font.className}>
+      <ThemeProvider attribute="class">
+        <Navbar />
+        <Component {...pageProps} />
+        <Analytics />
+      </ThemeProvider>
+    </main>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,7 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <Head />
       <body className="bg-jelly-bean-50 text-jelly-bean-950 dark:bg-jelly-bean-950 dark:text-jelly-bean-50">
         <Main />
         <NextScript />
