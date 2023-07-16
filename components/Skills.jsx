f393306b3b5890c77281c27d4f68beb0066e6f48
Emre Turan <itsemreturan@gmail.com>
2023-07-16 18:30:38 +0300

    small adj in skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
           <p className="text-xl tracking-widest uppercase text-jelly-bean-600 dark:text-jelly-bean-300">
             Skills
           </p>
-          <h2 className="py-4">What I Can Do</h2>
+          <h2 className="py-4">Tech Stack</h2>
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 dark:bg-jelly-bean-50 dark:text-jelly-bean-950">
               <div className="grid grid-cols-2 gap-4 justify-center items-center">
